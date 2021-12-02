738377607872a912382465b2df269ee96a29c7ef
sebastienHuet <huetsb@gmail.com>
2021-12-02 18:25:28 +0900

    update of bar menu and top img

--- resources/js/index.js
+++ resources/js/index.js
@@ -2,18 +2,21 @@ let body = document.querySelector('body');
 let element = document.documentElement;
 let logo = document.querySelector('.logo-company');
 let logoTitle = document.querySelector('.logo-company h1');
+let menu = document.querySelector('.menu-container')
 
 function scrollWindow (){
 if (body.scrollTop > 80 || element.scrollTop > 80){
-    logo.style.width = '150px'
-    logo.style.height = '150px'
-    logoTitle.style.fontSize = '1.5em'
+    logo.style.width = '100px'
+    logo.style.height = '100px'
+    logoTitle.style.fontSize = '1em'
     logoTitle.style.padding = '10px'
+    menu.style.backgroundColor = 'rgba(255,255,255,0.94)';
    
 }else{
     logo.style.width = '250px'
     logo.style.height = '250px'
     logoTitle.style.fontSize = '3em'
+    menu.style.backgroundColor = 'transparent';
 }
 }
 
